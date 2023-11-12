89f46ee1370be0689b37f2570be0793d386445b4
Eder venancio <ederpvenancio@gmail.com>
2023-11-12 18:27:42 -0300

    first commit

---
+++ src/main/java/com/escalationSystem/escalation/EscalationApplication.java
@@ -0,0 +1,13 @@
+package com.escalationSystem.escalation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalationApplication.class, args);
+	}
+
+}
